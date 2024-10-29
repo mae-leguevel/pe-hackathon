12a89c5892905444b6a76bb610322de598669cca
mae-leguevel <leguevelmaelig@gmail.com>
2024-10-29 15:22:16 +0100

    On test

--- .ipynb_checkpoints/notebook_maelig-checkpoint.py
+++ .ipynb_checkpoints/notebook_maelig-checkpoint.py
@@ -7,4 +7,15 @@
 #       format_name: percent
 #       format_version: '1.3'
 #       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
 # ---
+
+# %%
+from common import *
+
+# %%
+
+#df.iloc[:, df.getloc('1980'):df.getloc('2000')] = df.iloc[:,  df.getloc('1980'):df.getloc('2000')]].astype(float)
